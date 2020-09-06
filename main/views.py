b4039eac8744d6ea8d73efa8533bae7a261a7152
Abdullah Alam <abdullahalam598@gmail.com>
2020-09-06 12:57:10 +0530

    Basic post layout in place

--- main/views.py
+++ main/views.py
@@ -9,17 +9,20 @@ def index(request):
         {
             'header_image': static('/img/ladakh.jpg'),
             'title': 'Dont miss this cool Image!',
-            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?'
+            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?',
+            'slug': 'dummy-post'
         },
         {
             'header_image': static('/img/cats.jpg'),
             'title': 'Dont miss this cool Image!',
-            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam? Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia!'
+            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam? Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia!',
+            'slug': 'dummy-post'
         },
         {
             'header_image': static('/img/monkey.jpg'),
             'title': 'Dont miss this cool Image!',
-            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?'
+            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?',
+            'slug': 'dummy-post'
         }
     ]
 
@@ -47,14 +50,34 @@ def index(request):
  
 def post(request, slug):
     # FETCH OBJ
-    post_obj=Post.objects.get(slug = str(slug))
+    # post_obj=Post.objects.get(slug = str(slug))
+    post_obj = {
+            'header_image': static('/img/ladakh.jpg'),
+            'title': 'Dont miss this cool Image!',
+            'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?',
+            'content': '''<p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique cumque, impedit quos libero ducimus deserunt repellat, unde pariatur nostrum ratione non minus sit neque aspernatur consequatur eveniet! Animi, veniam consectetur!
+</p>
+<p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolor ducimus porro iure aperiam consectetur libero necessitatibus magni quae nesciunt doloremque eveniet hic sit commodi quas enim rerum, sequi dolores id adipisci nostrum, aspernatur fuga est error? Corporis harum velit laudantium atque error id commodi, a numquam dolorem minima accusamus vel laborum voluptate magnam enim nihil adipisci deleniti eos et?
+</p>
+<img data-src="/static/img/cats.jpg" class="lozad" />
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, architecto eaque asperiores saepe, nihil nisi, optio laborum blanditiis maxime impedit unde. Cum minus fugiat iure odio incidunt officiis magnam, nisi doloremque cupiditate laboriosam quod necessitatibus earum ut ducimus repellat repudiandae ipsa quo nulla quos. Deserunt architecto earum voluptas, fugiat magnam explicabo saepe? Minus iusto modi cum ab nesciunt sit dolorem adipisci nemo molestiae est. Ea saepe assumenda sit eveniet reprehenderit fugit aliquam dolor dolore dignissimos ratione, fugiat sunt earum corporis! Facilis, minus temporibus iste ex deserunt vel molestias, consequatur tempora ad eaque distinctio. Enim ipsam dolorem, error ad natus, iusto atque dignissimos voluptatum, cupiditate autem alias! Ducimus voluptates, rerum culpa porro accusantium modi incidunt, dolor deleniti placeat laborum, maiores dolorem.
+</p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus nobis fugiat. Delectus doloribus et aut quis nesciunt officiis optio sit ducimus illo similique repellendus consectetur exercitationem quos cupiditate impedit voluptate tempora quo facere voluptas dolorum, dignissimos laudantium earum? Animi porro incidunt, vero asperiores amet reiciendis illo voluptatem nesciunt iusto repudiandae autem corrupti! In, mollitia!
+</p>''',
+            'slug': 'dummy-post',
+        }
  
     # HUMAN FRIENDLY DATE
-    hfr_date = post_obj.created.strftime('%e %b %Y')
-    post_obj.hfr_date = hfr_date
+    # hfr_date = post_obj.created.strftime('%e %b %Y')
+    # post_obj.hfr_date = hfr_date
  
     # CREATE CONTEXT
     context = {
+        'header_image': post_obj['header_image'],
         'post': post_obj,
     }
  
