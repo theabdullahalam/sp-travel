38ab644b4580988fbe2dc2423481db6f067c832f
Abdullah Alam <abdullahalam598@gmail.com>
2020-09-09 00:44:22 +0530

    Posts list page designed

--- main/views.py
+++ main/views.py
@@ -3,6 +3,15 @@ from django.core.paginator import Paginator
 from .models import Post, PostType
 from django.templatetags.static import static
  
+def about(request):
+    header_image = static('img/mountains.jpg')
+
+    context = {
+        'header_image': header_image,
+    }
+
+    return render(request, 'about.html', context=context)
+
 def index(request):
     header_image = static('img/mountains.jpg')
     posts = [
@@ -56,19 +65,19 @@ def post(request, slug):
             'title': 'Dont miss this extra super cool Image!',
             'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?',
             'content': '''<p>
-    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique cumque, impedit quos libero ducimus deserunt repellat, unde pariatur nostrum ratione non minus sit neque aspernatur consequatur eveniet! Animi, veniam consectetur!
-</p>
-<p>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolor ducimus porro iure aperiam consectetur libero necessitatibus magni quae nesciunt doloremque eveniet hic sit commodi quas enim rerum, sequi dolores id adipisci nostrum, aspernatur fuga est error? Corporis harum velit laudantium atque error id commodi, a numquam dolorem minima accusamus vel laborum voluptate magnam enim nihil adipisci deleniti eos et?
-</p>
-<img data-src="/static/img/cats.jpg" class="lozad" />
-<div class="caption"><small><em>Cool Cats</em></small></div>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, architecto eaque asperiores saepe, nihil nisi, optio laborum blanditiis maxime impedit unde. Cum minus fugiat iure odio incidunt officiis magnam, nisi doloremque cupiditate laboriosam quod necessitatibus earum ut ducimus repellat repudiandae ipsa quo nulla quos. Deserunt architecto earum voluptas, fugiat magnam explicabo saepe? Minus iusto modi cum ab nesciunt sit dolorem adipisci nemo molestiae est. Ea saepe assumenda sit eveniet reprehenderit fugit aliquam dolor dolore dignissimos ratione, fugiat sunt earum corporis! Facilis, minus temporibus iste ex deserunt vel molestias, consequatur tempora ad eaque distinctio. Enim ipsam dolorem, error ad natus, iusto atque dignissimos voluptatum, cupiditate autem alias! Ducimus voluptates, rerum culpa porro accusantium modi incidunt, dolor deleniti placeat laborum, maiores dolorem.
-</p>
-<p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus nobis fugiat. Delectus doloribus et aut quis nesciunt officiis optio sit ducimus illo similique repellendus consectetur exercitationem quos cupiditate impedit voluptate tempora quo facere voluptas dolorum, dignissimos laudantium earum? Animi porro incidunt, vero asperiores amet reiciendis illo voluptatem nesciunt iusto repudiandae autem corrupti! In, mollitia!
-</p>''',
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique cumque, impedit quos libero ducimus deserunt repellat, unde pariatur nostrum ratione non minus sit neque aspernatur consequatur eveniet! Animi, veniam consectetur!
+                    </p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolor ducimus porro iure aperiam consectetur libero necessitatibus magni quae nesciunt doloremque eveniet hic sit commodi quas enim rerum, sequi dolores id adipisci nostrum, aspernatur fuga est error? Corporis harum velit laudantium atque error id commodi, a numquam dolorem minima accusamus vel laborum voluptate magnam enim nihil adipisci deleniti eos et?
+                    </p>
+                    <img data-src="/static/img/cats.jpg" class="lozad" />
+                    <div class="caption"><small><em>Cool Cats</em></small></div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, architecto eaque asperiores saepe, nihil nisi, optio laborum blanditiis maxime impedit unde. Cum minus fugiat iure odio incidunt officiis magnam, nisi doloremque cupiditate laboriosam quod necessitatibus earum ut ducimus repellat repudiandae ipsa quo nulla quos. Deserunt architecto earum voluptas, fugiat magnam explicabo saepe? Minus iusto modi cum ab nesciunt sit dolorem adipisci nemo molestiae est. Ea saepe assumenda sit eveniet reprehenderit fugit aliquam dolor dolore dignissimos ratione, fugiat sunt earum corporis! Facilis, minus temporibus iste ex deserunt vel molestias, consequatur tempora ad eaque distinctio. Enim ipsam dolorem, error ad natus, iusto atque dignissimos voluptatum, cupiditate autem alias! Ducimus voluptates, rerum culpa porro accusantium modi incidunt, dolor deleniti placeat laborum, maiores dolorem.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus repellendus nobis fugiat. Delectus doloribus et aut quis nesciunt officiis optio sit ducimus illo similique repellendus consectetur exercitationem quos cupiditate impedit voluptate tempora quo facere voluptas dolorum, dignissimos laudantium earum? Animi porro incidunt, vero asperiores amet reiciendis illo voluptatem nesciunt iusto repudiandae autem corrupti! In, mollitia!
+                    </p>''',
             'slug': 'dummy-post',
     }
 
@@ -86,8 +95,6 @@ def post(request, slug):
     # HUMAN FRIENDLY DATE
     # hfr_date = post_obj.created.strftime('%e %b %Y')
     # post_obj.hfr_date = hfr_date
-
-    print(comments)
  
     # CREATE CONTEXT
     context = {
@@ -99,10 +106,21 @@ def post(request, slug):
     # RETURN
     return render(request, 'post.html', context=context)
  
-def posts(request, pageno=1):
+def posts(request, category='all', pageno=1):
     # FETCH ALL POSTS
     # posts = Post.objects.filter(p_type__type_name = typename).exclude(slug='about').order_by('-created', 'title')
-    posts = Post.objects.all().order_by('-created', 'title')
+    # posts = Post.objects.all().order_by('-created', 'title')
+    header_image = static('img/car.jpg')
+    posts = []
+    post = {
+        'header_image': static('/img/ladakh.jpg'),
+        'title': 'Dont miss this cool Image!',
+        'preview': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi sapiente vel minima ex cumque qui esse eos mollitia veniam?',
+        'slug': 'dummy-post'
+    }
+
+    for i in range(0,40):
+        posts.append(post)
  
     # PAGINATE
     paginator = Paginator(posts, 10)
@@ -111,14 +129,16 @@ def posts(request, pageno=1):
     posts = page_obj.object_list
  
     # HUMAN FRIENDLY DATE
-    for post in posts:
-        hfr_date = post.created.strftime('%e %b %Y')
-        post.hfr_date = hfr_date
+    # for post in posts:
+    #     hfr_date = post.created.strftime('%e %b %Y')
+    #     post.hfr_date = hfr_date
  
-        post.preview = str(post.content).split('</p>')[0].split('<p>')[1]
+    #     post.preview = str(post.content).split('</p>')[0].split('<p>')[1]
  
     # SET CONTEXT
     context = {
+        'header_image': header_image,
+        'category': category,
         'posts': posts,
         'pageinator': paginator,
         'page_obj': page_obj,
